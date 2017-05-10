ac87a178168e2d04ce28ca199cfd394f6590c364
Eman <programmingeman@gmail.com>
2017-05-09 21:09:28 -0700

    timeout and checksum

---
+++ RdtUdpServer/timeout.hpp
@@ -0,0 +1,14 @@
+#ifndef TIMEOUT_HPP_INCLUDED
+#define TIMEOUT_HPP_INCLUDED
+
+#include <netinet/in.h>
+#include <stdint.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/time.h>
+#include "rdt.hpp"
+
+int callTimeout(int filedes, unsigned int seconds);
+
+#endif // TOUT_H_INCLUDED
